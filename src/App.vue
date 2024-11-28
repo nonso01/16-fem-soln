3762ad0ff166883537d2849e09517ac11e607642
nonso01 <chukwunonsomartin01@gmail.com>
2024-11-28 10:33:07 +0100

    setting up styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Order from "./components/Order.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex container bd">
+    <div class="A bd">
+      <Order />
+    </div>
+    <div class="B bd">B</div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.container {
+  padding: 2% 12%;
+  gap: 1rem;
+  .A {
+    flex: 0.7;
+  }
+  .B {
+    flex: 0.3;
+  }
 }
 </style>
---
+++ src/components/Order.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  cartItemNumber: Number,
+  epmty: Boolean,
+});
+</script>
+
+<template>
+  <div class="order">
+    <div>
+      <h3>Your Cart ( {{ cartItemNumber }} )</h3>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+h3 {
+  color: var(--red);
+}
+.order {
+  min-height: 200px;
+  width: 300px;
+  padding: 1%;
+  background-color: #fff;
+  border-radius: 0.625rem;
+}
+</style>
